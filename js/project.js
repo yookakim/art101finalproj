e23b3e4142601bc5b82b8cfb69349bab6ff46ad4
yookakim <yookakim7259@gmail.com>
2021-03-14 23:06:58 -0700

    add basic task list functionality

--- js/project.js
+++ js/project.js
@@ -6,17 +6,126 @@
 //var worktimerinput = document.getElementById("worktimerinput");
 //var breaktimerinput = document.getElementById("breaktimerinput");
 
+const taskItemTemplate = document.createElement('template');
+
+taskItemTemplate.innerHTML = `
+  <style>
+    div {
+      font-family: courier;
+    }
+    .taskcontainer {
+      display: flex;
+      padding-top: 5px;
+      padding-bottom: 5px;
+      border-top: darkgray;
+      border-top-width: 1px;
+      border-top-style: solid;
+    }
+    .tasktitle {
+      padding-right: 10px;
+    }
+    .taskbutton {
+      display: inline-block;
+    }
+  </style>
+  <div class="taskcontainer">
+    <div class="tasktitle">
+      Test Template
+    </div>
+    <button class="taskbutton">Remove Task</button>
+  </div>
+`;
+
+class TaskItem extends HTMLElement {
+  constructor(title, index) {
+    super();
+
+    this.attachShadow( {mode : 'open' });
+    this.shadowRoot.appendChild(taskItemTemplate.content.cloneNode(true));
+
+    this.title = title;
+    this.index = index;
+    this.priority;
+
+    this.shadowRoot.querySelector(".tasktitle").innerHTML = title;
+    this.shadowRoot.querySelector(".taskbutton").addEventListener('click', () => this.removeTask());
+  }
+
+  removeTask() {
+    console.log('removed task');
+    this.shadowRoot.querySelector(".tasktitle").innerHTML = 'test lol';
+    this.parentNode.removeChild(this);
+    taskList.splice(this.index);
+  }
+
+  toJSON() {
+    // when JSON.stringify() is called on this class object, it looks for this method first then takes the object it returns
+    // so we pass in the properties we need to build the task
+    return {
+      title: this.title,
+      index: this.index
+    }
+  }
+
+  connectedCallback() {
+    // this.shadowRoot.querySelector(".taskbutton").addEventListener('click', () => this.removeTask());
+  }
+}
+
+window.customElements.define('task-item', TaskItem);
+
+var taskList2 = ["test1", { test2: 'test2'}, 'test3'];
+var taskList = [];
 
 var worktimerinput = $('#worktimerinput');
 var breaktimerinput = $('#breaktimerinput');
+
+var tasktitleinput = $('#task-title-input');
 
 // Buttons
 var worktimerbutton = $('#worktimerbutton');
 var breaktimerbutton = $('#breaktimerbutton');
 var clearsessioncountbutton = $('#clearsessioncountbutton');
+var addtaskbutton = $('#add-task-button');
 
 var timerDisplay = $('#timerdisplay');
 var workSessionsDisplayCount = $('#totalworksessionscompleted');
+
+var tasklistContainer = $('#tasklist-container');
+
+function addTask(titleString) {
+  // create new task item/object
+  // add the task object to the global tasklist array
+  // parse the array to JSON and store to localStorage (immediately)
+  // call refresh function to update the display of task list in DOM?
+
+  // create the new task
+  var newTask = new TaskItem(titleString, taskList.length);
+
+  // add the new task to the list
+  taskList.push(newTask);
+
+  // parse the array with new added task and put in localStorage
+  window.localStorage.setItem('taskList', JSON.stringify(taskList));
+
+  // tasklistContainer.append(newTask);
+
+  refreshList();
+}
+
+function refreshList() {
+  // should it go through for loop and append each list object, or should it create the whole element and attach that instead?
+  // processing overhead to redraw entire list each time probably isn't that big of a deal
+
+  var newList = document.createElement('div');
+
+  for (let i = 0; i < taskList.length; i++) {
+    // newList.append(taskList[i]);
+    tasklistContainer.append(taskList[i]);
+  }
+
+  // tasklistContainer.replaceWith(newList);
+}
 
 
 function initializeAudio() {
@@ -27,7 +136,32 @@ function initializeAudio() {
   alarm.volume = 0.2;
 }
 
+function initializeStorageItems() {
+
+  // initialize work sessions count
+  if (!window.localStorage.hasOwnProperty('totalWorkSessions')) {
+    window.localStorage.setItem('totalWorkSessions', 0);
+    console.log('Empty local storage, initializing work session count');
+  } else {
+    workSessionsDisplayCount.text(getWorkSessions());
+  }
+
+  // initialize list of tasks
+  /*
+    if (list item doesn't exist in local storage) {
+      storeListToStorage();    // this would be another function defined somewhere, do setItem() and stringify the global taskList array before passing through 
+      or,
+      window.localStorage.setItem('taskList', )
+    } else {
+      // the list does exist in storage
+      updateList();            // reusable function that would refresh/update the task list?
+
+    }
+  */
+}
+
 function getWorkSessions() {
+  // since localStorage only stores data in the form of key <-> string pairs, change string to number before use/display
   return Number(window.localStorage.getItem('totalWorkSessions'));
 }
 
@@ -43,16 +177,27 @@ $(document).ready(() => {
 
   initializeAudio();
 
-  if (window.localStorage.getItem('totalWorkSessions') == null) {
-    window.localStorage.setItem('totalWorkSessions', 0);
-    console.log('Empty local storage, initializing work session count');
-  } else {
-    workSessionsDisplayCount.text(getWorkSessions());
-  }
+  initializeStorageItems();
+
+
+  // add existing tasks
+  /*
+    for (i is 0, i less than length of list of tasks in localStorage, i++) {
+      print each task in the taskList object in localStorage
+    }
+  */
+
   // window.localStorage.removeItem('totalWorkSessions');
 })
 
 const TIMER_INTERVAL = 100;
+
+addtaskbutton.click(function() {
+  var title = tasktitleinput.val();
+  addTask(title);
+})
+
+
 
 worktimerbutton.click(function() {
   var timeLength = worktimerinput.val();
